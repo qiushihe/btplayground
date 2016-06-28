73eb1d8330eae95a0751c105f8e1a3786d9173ea
Billy He <qiushihe@me.com>
2016-06-27 23:10:10 -0700

    Some refactors

--- sobt/bencoding.swift
+++ sobt/bencoding.swift
@@ -9,22 +9,55 @@
 import Swift;
 import Foundation
 
-let BYTE_COLON: UInt8 = 58;
-let BYTE_LC_E: UInt8 = 101;
+func identity(arg: Any?) -> Any? {
+  return arg;
+}
 
-let BYTE_ZERO: UInt8 = 48;
-let BYTE_NINE: UInt8 = 57;
-let BYTE_LC_I: UInt8 = 105;
-let BYTE_LC_L: UInt8 = 108;
-let BYTE_LC_D: UInt8 = 100;
+func flow(functions: Array<(Any?) -> Any?>) -> ((Any?) -> Any?) {
+  return functions.count <= 0
+    ? identity
+    : {(arg: Any?) -> Any? in
+      return flow(
+        Array<(Any?) -> Any?>(functions.count == 1
+          ? []
+          : functions[1...(functions.count - 1)])
+        )(functions[0](arg));
+    };
+}
 
 extension NSData {
   func getBytes() -> Array<UInt8> {
     let size = sizeof(UInt8);
     let count = self.length / size;
     var bytes = Array<UInt8>.init(count: count, repeatedValue: 0);
-    self.getBytes(&bytes, length:count * size);
+    self.getBytes(&bytes, length:bytes.count * size);
     return bytes;
+  }
+}
+
+enum BEncodedSeparator: UInt8 {
+  case Colon = 58; // :
+  case End = 101;  // e
+}
+
+enum BEncodedDataType {
+  case String, Integer, List, Dictionary
+}
+
+enum BEncodedDataTypeByte: UInt8 {
+  case _0 = 48, _1, _2, _3, _4, _5, _6, _7, _8, _9; // 0 - 9
+  case _lc_d = 100;                                 // d
+  case _lc_i = 105;                                 // i
+  case _lc_l = 108;                                 // l
+
+  var dataType: BEncodedDataType {
+    switch self {
+    case ._0, _1, _2, _3, _4, _5, _6, _7, _8, _9:
+      return BEncodedDataType.String;
+    case ._lc_i: return BEncodedDataType.Integer;
+    case ._lc_l: return BEncodedDataType.List;
+    case ._lc_d: return BEncodedDataType.Dictionary;
+    }
   }
 }
 
@@ -35,7 +68,7 @@ enum BEncoded {
   case Dictionary(Swift.Dictionary<Swift.String, BEncoded>)
   
   var type: Swift.String {
-    switch(self) {
+    switch self {
     case .String: return "string";
     case .Integer: return "integer";
     case .List: return "list";
@@ -44,7 +77,7 @@ enum BEncoded {
   }
   
   var value: Any {
-    switch(self) {
+    switch self {
     case .String(let stringValue): return stringValue;
     case .Integer(let integerValue): return integerValue;
     case .List(let listValue): return listValue;
@@ -52,35 +85,34 @@ enum BEncoded {
     }
   }
   
+  func indents(level: Int = 0) -> Swift.String {
+    return Swift.String.init(count: level * 2, repeatedValue: Character(" "));
+  }
+  
   func pp(level: Int = 0, noIndent: Bool = false) {
-    switch(self) {
+    switch self {
     case .String(let stringValue):
-      let indents = Swift.String.init(count: level * 2, repeatedValue: Character(" "));
       let ppString = "\"" + stringValue + "\"";
-      print(noIndent ? ppString : indents + ppString);
+      print(noIndent ? ppString : self.indents(level) + ppString);
       break;
     case .Integer(let integerValue):
-      let indents = Swift.String.init(count: level * 2, repeatedValue: Character(" "));
       let ppString = Swift.String.init(integerValue);
-      print(noIndent ? ppString : indents + ppString);
+      print(noIndent ? ppString : self.indents(level) + ppString);
       break;
     case .List(let listValue):
-      let rootIndents = Swift.String.init(count: level * 2, repeatedValue: Character(" "));
-      print(noIndent ? "[" : rootIndents + "[");
+      print(noIndent ? "[" : self.indents(level) + "[");
       for value in listValue {
         value.pp(level + 1);
       }
-      print(rootIndents + "]");
+      print(self.indents(level) + "]");
       break;
     case .Dictionary(let dictionaryValue):
-      let rootIndents = Swift.String.init(count: level * 2, repeatedValue: Character(" "));
-      let valueIndents = Swift.String.init(count:(level + 1) * 2, repeatedValue: Character(" "));
-      print(noIndent ? "{" : rootIndents + "{");
+      print(noIndent ? "{" : self.indents(level) + "{");
       for (key, value) in dictionaryValue {
-        print(valueIndents + "\"" + key + "\": ", terminator: "");
+        print(self.indents(level + 1) + "\"" + key + "\": ", terminator: "");
         value.pp(level + 1, noIndent: true);
       }
-      print(rootIndents + "}");
+      print(self.indents(level) + "}");
       break;
     }
   }
@@ -94,12 +126,12 @@ func getLength(ofString string: String) -> Int {
   return string.lengthOfBytesUsingEncoding(NSUTF8StringEncoding);
 }
 
-func readString(fromBytes bytes: Array<UInt8>, startAt start: Int, stopBefore stopByte: UInt8) -> String {
+func readString(fromBytes bytes: Array<UInt8>, startAt start: Int, stopBefore stopByte: BEncodedSeparator) -> String {
   var str = "";
   var position = start;
   
   while true {
-    if bytes[position] == stopByte {
+    if BEncodedSeparator(rawValue: bytes[position]) == stopByte {
       break;
     }
     str += getString(fromByte: bytes[position]);
@@ -116,7 +148,7 @@ func readString(fromBytes bytes: Array<UInt8>, startAt start: Int, stopAfterLeng
 func bDecodeString(fromBytes bytes: Array<UInt8>, startIndex start: Int, inout nextIndex position: Int) -> BEncoded {
   position = start;
   
-  let lengthStr = readString(fromBytes: bytes, startAt: position, stopBefore: BYTE_COLON);
+  let lengthStr = readString(fromBytes: bytes, startAt: position, stopBefore: BEncodedSeparator.Colon);
   position += getLength(ofString: lengthStr) + 1;
   
   let length = Int.init(lengthStr)!;
@@ -129,7 +161,7 @@ func bDecodeString(fromBytes bytes: Array<UInt8>, startIndex start: Int, inout n
 func bDecodeInteger(fromBytes bytes: Array<UInt8>, startIndex start: Int, inout nextIndex position: Int) -> BEncoded {
   position = start + 1;
   
-  let integerStr = readString(fromBytes: bytes, startAt: position, stopBefore: BYTE_LC_E);
+  let integerStr = readString(fromBytes: bytes, startAt: position, stopBefore: BEncodedSeparator.End);
   let integer = Int.init(integerStr)!;
   position += getLength(ofString: integerStr) + 1;
   
@@ -142,10 +174,10 @@ func bDecodeList(fromBytes bytes: Array<UInt8>, startIndex start: Int, inout nex
   var list: Array<BEncoded> = [];
   
   while true {
-    if bytes[position] == BYTE_LC_E {
+    if BEncodedSeparator(rawValue: bytes[position]) == BEncodedSeparator.End {
       break;
     }
-    
+
     let decoded: BEncoded = bDecode(bytes, startIndex: position, nextIndex: &position);
     list.append(decoded);
   }
@@ -161,7 +193,7 @@ func bDecodeDictionary(fromBytes bytes: Array<UInt8>, startIndex start: Int, ino
   var dictionary: Dictionary<String, BEncoded> = [:];
   
   while true {
-    if bytes[position] == BYTE_LC_E {
+    if BEncodedSeparator(rawValue: bytes[position]) == BEncodedSeparator.End {
       break;
     }
     
@@ -183,19 +215,16 @@ func bDecodeDictionary(fromBytes bytes: Array<UInt8>, startIndex start: Int, ino
 }
 
 func bDecode(bytes: Array<UInt8>, startIndex start: Int, inout nextIndex position: Int) -> BEncoded {
-  let firstByte = bytes[start];
-  
-  if firstByte >= BYTE_ZERO && firstByte <= BYTE_NINE {
+  switch BEncodedDataTypeByte(rawValue: bytes[start])!.dataType {
+  case BEncodedDataType.String:
     return bDecodeString(fromBytes: bytes, startIndex: start, nextIndex: &position);
-  } else if firstByte == BYTE_LC_I {
+  case BEncodedDataType.Integer:
     return bDecodeInteger(fromBytes: bytes, startIndex: start, nextIndex: &position);
-  } else if firstByte == BYTE_LC_L {
+  case BEncodedDataType.List:
     return bDecodeList(fromBytes: bytes, startIndex: start, nextIndex: &position);
-  } else if firstByte == BYTE_LC_D {
+  case BEncodedDataType.Dictionary:
     return bDecodeDictionary(fromBytes: bytes, startIndex: start, nextIndex: &position);
   }
-  
-  return BEncoded.String("WTF");
 }
 
 func bDecode(data: NSData!) -> BEncoded {

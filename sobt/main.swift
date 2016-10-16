568ef1cdf2d88fab5370bf14f75474f53cb5f2af
Billy He <qiushihe@me.com>
2016-10-16 15:10:30 -0700

    Add magnetlink sample

--- sobt/main.swift
+++ sobt/main.swift
@@ -21,6 +21,9 @@ print(jsonObject);
 // http://www.bittorrent.org/beps/bep_0015.html
 // http://www.rasterbar.com/products/libtorrent/udp_tracker_protocol.html
 
+// magnet:?xt=urn:btih:<info-hash>&dn=<name>&tr=<tracker-url>&x.pe=<peer-address>
+// magnet:?xt=urn:btih:f36ccb2248d556663e18490d679b5d914a7e8f63&tr=udp://127.0.0.1:4242
+
 let trackerClient = Sobt.TrackerClient.TrackerClient();
 trackerClient.addManifest(fromPath: "/Users/billy/Projects/btplayground/test.torrent");
 trackerClient.setPort(4321);
